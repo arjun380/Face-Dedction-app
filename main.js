7865dbbf631082f509ec42b87e2d3730429e094e
arjun380 <82092827+arjun380@users.noreply.github.com>
2021-09-17 09:39:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+https://teachablemachine.withgoogle.com/models/xAVRKRmIP/
+Webcam.set(
+    {
+        width: 350,
+        height:300,
+        image_format:'png',
+        png_quality:90
+    });
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function snapshort()
+{ Webcam.snap(function(data_uri){
+         document.getElementById("result").innerHTML='<img id="captured_img" src="'+data_uri+'">';
+          });
+}console.log("ml5 version",ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/4l31CuQpp/model.json',modelLoaded);
+function modelLoaded()
+{
+    console.log("model is loaded");
+}
+
+function filtering()
+{ 
+    img = document.getElementById('captured_img'); 
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error, results)
+{ 
+    if(error) {
+        console.error(error);
+    }else {
+        console.log(results);
+        document.getElementById("result_objct_name").innerHTML = results[0].label;
+        document.getElementById("result_objct_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        
+    }
+}
